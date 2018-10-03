cf37cdf593a68d0e367f9aa81a2481f1065f173b
Jessica Prieto <jessicaprieto31@gmail.com>
2018-10-03 16:31:48 -0400

    removed re-routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { Header }           from './Header.js';
+import { AboutMe }          from './AboutMe.js';
+import { SkillHighlights }  from './SkillHighlights.js';
+import { FeaturedProjects } from './FeaturedProjects.js';
+import { FeaturedBlog }     from './FeaturedBlog.js';
+import { Instagram }        from './Instagram.js'
+import { ContactInfo }      from './ContactInfo.js'; 
 
-import { LandingPage } from './LandingPage';
-import { BlogPage } from './BlogPage';
-import { AllBlogs } from './AllBlogs';
-import { Projects } from './Projects';
+// import { LandingPage } from './LandingPage';
+// import { BlogPage } from './BlogPage';
+// import { AllBlogs } from './AllBlogs';
+// import { Projects } from './Projects';
 
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -17,68 +24,77 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    var sites = ['http://localhost:3000/', 'https://jessicaprt.github.io/personal-website-react/', 'https://test.jessica.prieto.ca/'];
+  // componentDidMount() {
+  //   var sites = ['http://localhost:3000/', 'https://jessicaprt.github.io/personal-website-react/', 'https://test.jessica.prieto.ca/'];
     
-    if ( !sites.includes(window.location.href) ) {
-      axios.get('./blogs.json')
-          .then ((response) => {
-              const blogs = response.data.blogs;
-              this.setState({ blogs }) })
+  //   if ( !sites.includes(window.location.href) ) {
+  //     axios.get('./blogs.json')
+  //         .then ((response) => {
+  //             const blogs = response.data.blogs;
+  //             this.setState({ blogs }) })
 
-          .catch((error) => {
-            // Error
-            if (error.response) {
-              console.log("blogs.json shouldn't load here");
-            }
-          } );
-    }
+  //         .catch((error) => {
+  //           // Error
+  //           if (error.response) {
+  //             console.log("blogs.json shouldn't load here");
+  //           }
+  //         } );
+  //   }
     
-  }
+  // }
   
   render() {
     return (
-      <BrowserRouter>
-      <div>
-        <Route exact={true} 
-          path='/' 
-          baseName='/personal-website-react'
-          render={() => (
-            <div className="App">
-              <LandingPage />
-            </div>
-        )}/>
-
-        <Route exact={true} 
-          path='/blogs/all_blogs'
-          baseName='/personal-website-react/blogs/all_blogs'
-          render={() => (
-            <div className="App">
-              <AllBlogs />
-            </div>
-        )}/>
-
-        <Route exact={true} 
-          path='/projects' 
-          baseName='/projects'
-          render={() => (
-            <div className="App">
-              <Projects />
-            </div>
-        )}/>
-
-        { this.state.blogs.map((blog) => 
-          <Route exact={true} 
-          path={'/blogs/' + blog.url} 
-          baseName={'/personal-website-react/blogs' + blog.url}
-          render={() => (
-            <div className="App">
-              <BlogPage blog={ blog }/>
-            </div> )}/>
-          ) }
-
+      <div className="App">
+        <Header />
+        <AboutMe />
+        <SkillHighlights />
+        <FeaturedProjects />
+        <FeaturedBlog />
+        <Instagram />
+        <ContactInfo />
       </div>
-    </BrowserRouter>
+    //   <BrowserRouter>
+    //   <div>
+    //     <Route exact={true} 
+    //       path='/' 
+    //       baseName='/personal-website-react'
+    //       render={() => (
+    //         <div className="App">
+    //           <LandingPage />
+    //         </div>
+    //     )}/>
+
+    //     <Route exact={true} 
+    //       path='/blogs/all_blogs'
+    //       baseName='/personal-website-react/blogs/all_blogs'
+    //       render={() => (
+    //         <div className="App">
+    //           <AllBlogs />
+    //         </div>
+    //     )}/>
+
+    //     <Route exact={true} 
+    //       path='/projects' 
+    //       baseName='/projects'
+    //       render={() => (
+    //         <div className="App">
+    //           <Projects />
+    //         </div>
+    //     )}/>
+
+    //     { this.state.blogs.map((blog) => 
+    //       <Route exact={true} 
+    //       path={'/blogs/' + blog.url} 
+    //       baseName={'/personal-website-react/blogs' + blog.url}
+    //       render={() => (
+    //         <div className="App">
+    //           <BlogPage blog={ blog }/>
+    //         </div> )}/>
+    //       ) }
+
+    //   </div>
+    // </BrowserRouter>
     );
   }
 }
