508bd654590031454397b120ea3887586d4c7697
Jessica Prieto <jessicaprieto31@gmail.com>
2018-10-14 20:44:36 -0400

    Updates

--- app-jquery.js
+++ app-jquery.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $("a").on('click', function(event) {
+    $(".header > .Navigation .mainNav .container .navbar-collapse .navbar-nav > .linkItem > a")
+        .on('click', function(event) {
 		var valid_hashes = ["#about-me", "#projects", "#blog", "#instagram", "#contact"];
 		if (jQuery.inArray(this.hash, valid_hashes) != -1) {
 			console.log(this.hash);
@@ -12,22 +13,6 @@ $(document).ready(function(){
 			});
 		}
 	});
-
-    $(window).scroll( function(){
-        $('.hide-element').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({
-                    'opacity':'1'			
-                }, 500);
-                
-                $(this).removeClass("hide-element");
-            }
-        }); 
-    }); 
 });
 
 $(window).scroll(function() {
